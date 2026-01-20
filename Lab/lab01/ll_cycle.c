ec76fb74f04873159dcbffbf566ac5fa85654b92
misaka <smileMisaka@outlook.com>
2026-01-20 18:32:17 +0800

    ans(lab01): add implementation of ll_has_cycle and pass the test

--- Lab/lab01/ll_cycle.c
+++ Lab/lab01/ll_cycle.c
@@ -2,6 +2,18 @@
 #include <stddef.h>
 
 int ll_has_cycle(node *head) {
-  /* your code here */
-  return 0;
+  node *tortoise = head;
+  node *hare = head;
+  while (1) {
+    for (int i = 0; i < 2; ++i) {
+      if (hare == NULL)
+        return 0;
+      hare = hare->next;
+    }
+    // NOTE: we don't need to check whether tortoise is NULL
+    // since the node is checked when updating hare
+    tortoise = tortoise->next;
+    if (hare == tortoise)
+      return 1;
+  }
 }
