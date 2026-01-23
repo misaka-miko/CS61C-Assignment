eff45afae23033d541abe32907ad6abfbcce4d1c
misaka <smileMisaka@outlook.com>
2026-01-23 21:58:48 +0800

    ans(lab02): add solution to exercise 2

--- Lab/lab02/lfsr.c
+++ Lab/lab02/lfsr.c
@@ -1,10 +1,11 @@
-#include <stdio.h>
+#include "lfsr.h"
 #include <stdint.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "lfsr.h"
 
 void lfsr_calculate(uint16_t *reg) {
-    /* YOUR CODE HERE */
+  uint16_t new_bit = ((*reg >> 0) & 1) ^ ((*reg >> 2) & 1) ^ ((*reg >> 3) & 1) ^
+                     ((*reg >> 5) & 1);
+  *reg = (*reg >> 1) | (new_bit << 15);
 }
-
