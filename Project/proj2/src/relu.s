b28e365e45357adbc27cf2062c44699567d7b3ca
misaka <smileMisaka@outlook.com>
2026-02-05 00:25:43 +0800

    feat(relu): add implementation of relu

--- Project/proj2/src/relu.s
+++ Project/proj2/src/relu.s
@@ -15,24 +15,29 @@
 relu:
     # Prologue
 
+  # check whether a1 is less than 1
+  # if is exit with code 78
+  blt zero, a1, loop_start
+  li a1, 78
+  jal exit2
 
 loop_start:
-    
-
-
-
-
-
-
+  # if a1 decrement to zero, simply jump to loop_end
+  beq a1, zero, loop_end
+  lw t0, 0(a0)
+  # if t0 >= 0, then simply increment a0 by 4, and decrement a1 by 1, jump back to loop_start
+  # if t0 < 0, jump to loop_continue for futher processing
+  bge t0, zero, loop_continue
+  sw zero, 0(a0)
 
 loop_continue:
-
-
+  addi a0, a0, 4
+  addi a1, a1, -1
+  j loop_start
 
 loop_end:
 
 
     # Epilogue
 
-    
 	ret
